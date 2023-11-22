862e1be0ce319e26f97b5dbde4695ff28eceea76
iisaacc <isaacporras2001@gmail.com>
2023-11-22 14:04:57 +0100

    philo

--- Rank03/philosophers/philo/aux.c
+++ Rank03/philosophers/philo/aux.c
@@ -1,34 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   aux.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isporras <isporras@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/21 11:07:38 by isporras          #+#    #+#             */
-/*   Updated: 2023/11/22 11:52:28 by isporras         ###   ########.fr       */
+/*   Created: 2023/11/21 13:34:26 by isporras          #+#    #+#             */
+/*   Updated: 2023/11/22 11:55:10 by isporras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <pthread.h>
-# include <stdio.h>
-# include <sys/time.h>
-# include <inttypes.h>
-
-typedef struct s_philo
+void	ft_print_stamp(void)
 {
-	int		n_phi;
-	int64_t	to_die;
-	int64_t	to_eat;
-	int64_t	to_sleep;
-	int		times_eat;
-} t_philo;
+	struct timeval	tv;
+	int64_t			time;
 
-void	ft_init(t_philo *ph, char **argv, int argc);
-void	print_philo(t_philo *ph);
-void	ft_print_stamp(void);
-
-#endif
+	gettimeofday(&tv, NULL);
+	time = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+	printf("%" PRId64 " ", time);
+}
--- Rank03/philosophers/philo/threads.c
+++ Rank03/philosophers/philo/threads.c
@@ -1,34 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   threads.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isporras <isporras@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/21 11:07:38 by isporras          #+#    #+#             */
-/*   Updated: 2023/11/22 11:52:28 by isporras         ###   ########.fr       */
+/*   Created: 2023/11/22 12:06:08 by isporras          #+#    #+#             */
+/*   Updated: 2023/11/22 12:11:34 by isporras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <pthread.h>
-# include <stdio.h>
-# include <sys/time.h>
-# include <inttypes.h>
-
-typedef struct s_philo
+void	*ft_init_threads(t_philo *ph)
 {
-	int		n_phi;
-	int64_t	to_die;
-	int64_t	to_eat;
-	int64_t	to_sleep;
-	int		times_eat;
-} t_philo;
-
-void	ft_init(t_philo *ph, char **argv, int argc);
-void	print_philo(t_philo *ph);
-void	ft_print_stamp(void);
-
-#endif
+	pthread_create()
+	return (ph);
+}
