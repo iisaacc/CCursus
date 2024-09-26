0f1b0328c42867f073f00a338d1d72dea4083358
iisaacc <isaacporras2001@gmail.com>
2024-09-26 12:25:55 +0200

    psuh

--- Rank05/Inception/srcs/requirements/mariadb/tools/init_db.sh
+++ Rank05/Inception/srcs/requirements/mariadb/tools/init_db.sh
@@ -13,10 +13,13 @@ until mysql -u "root" -p"${MYSQL_ROOT_PASSWORD}" -e ""; do
 done
 
 # Ejecuta comandos para configurar la base de datos
-mysql -u root -p"${ROOT_PASS} << EOF
+mysql -u root -p"${ROOT_PASS}" << EOF
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';
 FLUSH PRIVILEGES;
 EOF
+
+# Detener el servicio de MariaDB si es necesario
+service mysql stop
